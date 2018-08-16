f47ce882a36db2652007c692980ba7b9b71095de
Hyunrae Cho <9beach@gmail.com>
2018-08-16 21:31:43 +0900

    Make scripts concise

--- test/make-cli-test-output.sh
+++ test/make-cli-test-output.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-cat test/cli-test.in | lib/cli.js -d 2>&1 | cat - > test/cli-test.out.d
-cat test/cli-test.in | lib/cli.js -p 2>&1 | cat - > test/cli-test.out.p
-cat test/cli-test.in | lib/cli.js -j 2>&1 | cat - > test/cli-test.out.j
-cat test/cli-test.in | lib/cli.js -a 2>&1 | cat - > test/cli-test.out.a
+cat test/cli-test.in | lib/cli.js -d &> test/cli-test.out.d
+cat test/cli-test.in | lib/cli.js -p &> test/cli-test.out.p
+cat test/cli-test.in | lib/cli.js -j &> test/cli-test.out.j
+cat test/cli-test.in | lib/cli.js -a &> test/cli-test.out.a
