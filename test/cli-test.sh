bfb6dec6e9ef758ac6588c70facaee906734453d
Hyunrae Cho <9beach@gmail.com>
2018-09-03 11:47:34 +0900

    Debugging Travis CI failure

--- test/cli-test.sh
+++ test/cli-test.sh
@@ -2,7 +2,7 @@
 
 set -e
 
-cat README.md | lib/cli.js > /dev/null 2>&1
+cat README.md | lib/cli.js
 cat test/cli-test.in | lib/cli.js -d 2>&1 | diff - test/cli-test.out.d
 cat test/cli-test.in | lib/cli.js -p 2>&1 | diff - test/cli-test.out.p
 cat test/cli-test.in | lib/cli.js -j 2>&1 | diff - test/cli-test.out.j
