15aadda61b912a84fb349d5666d236202660eb99
9beach <9beach@gmail.com>
2020-05-24 18:14:53 +0900

    Patch vulnerabe version (minimist < 0.2.1) and make `--daum` default

--- test/cli-test.sh
+++ test/cli-test.sh
@@ -3,7 +3,6 @@
 set -e
 
 cat README.md | lib/cli.js > /dev/null 2>&1
-cat test/cli-test.in | lib/cli.js -d 2>&1 | diff - test/cli-test.out.d
-cat test/cli-test.in | lib/cli.js -p 2>&1 | diff - test/cli-test.out.p
-cat test/cli-test.in | lib/cli.js -j 2>&1 | diff - test/cli-test.out.j
-cat test/cli-test.in | lib/cli.js -a 2>&1 | diff - test/cli-test.out.a
+cat test/cli-test.in | lib/cli.js -d 2>&1 | diff - test/cli-test.out.daum
+cat test/cli-test.in | lib/cli.js -p 2>&1 | diff - test/cli-test.out.pnu
+cat test/cli-test.in | lib/cli.js -a 2>&1 | diff - test/cli-test.out.all
--- test/make-cli-test-output.sh
+++ test/make-cli-test-output.sh
@@ -1,6 +1,5 @@
 #!/usr/bin/env bash
 
-cat test/cli-test.in | lib/cli.js -d &> test/cli-test.out.d
-cat test/cli-test.in | lib/cli.js -p &> test/cli-test.out.p
-cat test/cli-test.in | lib/cli.js -j &> test/cli-test.out.j
-cat test/cli-test.in | lib/cli.js -a &> test/cli-test.out.a
+cat test/cli-test.in | lib/cli.js -d &> test/cli-test.out.daum
+cat test/cli-test.in | lib/cli.js -p &> test/cli-test.out.pnu
+cat test/cli-test.in | lib/cli.js -a &> test/cli-test.out.all
