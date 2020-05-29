85da89f8863a445de11c0906c358a46fcaf485e8
9beach <9beach@gmail.com>
2020-05-29 14:02:51 +0900

    Split cli-test.sh outputs to stdouts and stderrs

--- test/make-cli-test-output.sh
+++ test/make-cli-test-output.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 
-cat test/cli-test.in | lib/cli.js -d &> test/cli-test.out.daum
-cat test/cli-test.in | lib/cli.js -p &> test/cli-test.out.pnu
-cat test/cli-test.in | lib/cli.js -a &> test/cli-test.out.all
+cat test/cli-test.in | lib/cli.js -d > test/cli-test.out.daum 2> test/cli-test.err.daum
+cat test/cli-test.in | lib/cli.js -p > test/cli-test.out.pnu 2> test/cli-test.err.pnu
+cat test/cli-test.in | lib/cli.js -a > test/cli-test.out.all 2> test/cli-test.err.all
