cc0dbcd6d585915a7a2f679f17be8b301280a390
9beach <9beach@gmail.com>
2022-09-07 16:41:20 +0900

    Apply PNU info changes

--- test/pnu-spell-check.test.js
+++ test/pnu-spell-check.test.js
@@ -18,7 +18,7 @@ describe('spellCheckByPNU', () => {
     const sentence = `여름 저녁이 푸르를 때\t난 가리라
 보리 무성한 사이\t\t\t가느다란 풀 짓밟힌 샛길 속으로.
 몽상가인 난, 발에 신선함을 느끼리라.
-난 내 벗은 머리를   바람이 스쳐지나가게 내버려두리라.
+난 내 벗은 머리를 바람이 스쳐지나가게 내버려두리라.
 
 난 말하지 않으련다, 아무것도 생각지 않으련다.
 그러나 한없는 사랑이 내 혼속에 살아오리니
@@ -35,14 +35,6 @@ describe('spellCheckByPNU', () => {
       assert.equal(data[2].suggestions[0], '혼 속에');
       assert.equal(data[3].token, '행복되게');
       assert.equal(data[3].suggestions[0], '행복하게');
-    };
-    spellCheck(sentence, timeout, check, done, never);
-  });
-  it('should have 5 suggestions', (done) => {
-    const sentence = '마이골';
-    const timeout = 4000;
-    const check = (data) => {
-      assert.equal(data[0].suggestions.length, 5);
     };
     spellCheck(sentence, timeout, check, done, never);
   });
